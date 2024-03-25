3049e7e8377d17e6a0c33911626ff79d4acd377c
DannySamy3 <danielntunduye@gmail.com>
2024-03-25 16:44:44 +0300

    changev1

---
+++ app/Components/nav.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+("use client");
+
+const nav = () => {
+  return (
+    <section className="navbar-start">
+      <h2> YOUR PROJECTS</h2>
+      <button className="btn">Add Project</button>
+    </section>
+  );
+};
+
+export default nav;
