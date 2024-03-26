a93ec9bdffb3176a9cab7f02c255f4df1402d1d1
DannySamy3 <danielntunduye@gmail.com>
2024-03-26 17:23:33 +0300

    changes

--- app/Components/HomePage.tsx
+++ app/Components/HomePage.tsx
@@ -9,16 +9,16 @@ const HomePage = () => {
 
   return (
     <div className="w-4/5 mx-auto my-44 ">
-      <div className="flex flex-col items-center ">
+      <div className="flex flex-col items-center  ">
         <Image
           src="/no-projects.png"
           alt="notebook image"
           width={90}
           height={90}
-          className="mb-4"
+          className="mb-6 mr-20"
         />
         <p
-          className="text-stone-500 text-2xl font-bold mb-6
+          className="text-stone-500 text-2xl font-bold mb-6 mr-14
         "
         >
           No Project Selected
