4e61efaafc2cf1aceefd19aa61acd097ef1d5893
DannySamy3 <danielntunduye@gmail.com>
2024-03-29 02:55:20 -0700

    more changes

--- app/Components/NewTask.tsx
+++ app/Components/NewTask.tsx
@@ -14,19 +14,31 @@ const NewTask = ({ handleDelete, handleAdd }) => {
     setEnteredTask('');
   };
   return (
-    <div className='flex gap-9 mr-36 '>
+    <div className='flex justify-between  '>
       <input
         type='text'
-        className='input input-bordered w-64 px-2
+        className='input input-bordered w-1/2 px-2
             py-1  bg-stone-200 h-10'
         onChange={handleChange}
         value={enteredTask}
       />
       <button
         onClick={handleClick}
-        className='text-stone-300 btn btn-neutral hover:text-stone-500'
+        className='
+        hover:text-green-500'
       >
-        Add Task
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          viewBox='0 0 24 24'
+          fill='currentColor'
+          class='w-6 h-6'
+        >
+          <path
+            fill-rule='evenodd'
+            d='M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 9a.75.75 0 0 0-1.5 0v2.25H9a.75.75 0 0 0 0 1.5h2.25V15a.75.75 0 0 0 1.5 0v-2.25H15a.75.75 0 0 0 0-1.5h-2.25V9Z'
+            clip-rule='evenodd'
+          />
+        </svg>
       </button>
     </div>
   );
--- app/Components/SelectedProjected.tsx
+++ app/Components/SelectedProjected.tsx
@@ -15,29 +15,45 @@ const SelectedProject = ({
 
   return (
     <div className=' w-4/5 my-44 flex flex-col items-center pr-8 '>
-      <header
-        className='pb-4  mb-4 border-b-2 border-gray-700
-      '
-      >
-        <div className='flex items-center gap-96 '>
-          <h1
-            className='text-3xl font-bold text-gray-700
-          mb-2'
-          >
-            {project.title}
-          </h1>
-          <button
-            onClick={handleDelete}
-            className='border-none btn btn-neutral hover:bg-gray-300
-          hover:text-gray-900'
-          >
-            Delete
-          </button>
-        </div>
-        <p className='mb-4 text-black'>{formattedDate}</p>
-        <p className='text-black whitespace-pre-wrap'>{project.description}</p>
-      </header>
-      <Task deleter={handleDeleteTask} adder={handleAddTask} tasks={tasks} />
+      <div className='w-9/12'>
+        <header
+          className='pb-4  mb-4 border-b-2 border-gray-700
+        '
+        >
+          <div className='flex justify-between items-center mb-6'>
+            <h1
+              className='text-3xl font-bold text-gray-700
+            mb-2'
+            >
+              {project.title}
+            </h1>
+            <button
+              onClick={handleDelete}
+              className='
+            hover:text-red-500'
+            >
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                viewBox='0 0 24 24'
+                fill='currentColor'
+                class='w-8 h-8 hover:stroke-red-500'
+              >
+                <path d='M3.375 3C2.339 3 1.5 3.84 1.5 4.875v.75c0 1.036.84 1.875 1.875 1.875h17.25c1.035 0 1.875-.84 1.875-1.875v-.75C22.5 3.839 21.66 3 20.625 3H3.375Z' />
+                <path
+                  fill-rule='evenodd'
+                  d='m3.087 9 .54 9.176A3 3 0 0 0 6.62 21h10.757a3 3 0 0 0 2.995-2.824L20.913 9H3.087Zm6.133 2.845a.75.75 0 0 1 1.06 0l1.72 1.72 1.72-1.72a.75.75 0 1 1 1.06 1.06l-1.72 1.72 1.72 1.72a.75.75 0 1 1-1.06 1.06L12 15.685l-1.72 1.72a.75.75 0 1 1-1.06-1.06l1.72-1.72-1.72-1.72a.75.75 0 0 1 0-1.06Z'
+                  clip-rule='evenodd'
+                />
+              </svg>
+            </button>
+          </div>
+          <p className='mb-4 text-black'>{formattedDate}</p>
+          <p className='text-black whitespace-pre-wrap'>
+            {project.description}
+          </p>
+        </header>
+        <Task deleter={handleDeleteTask} adder={handleAddTask} tasks={tasks} />
+      </div>
     </div>
   );
 };
