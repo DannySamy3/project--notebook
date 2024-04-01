d33c77be37bd5e41c2c3b531b6660778ef74cce6
DannySamy3 <danielntunduye@gmail.com>
2024-04-01 12:16:57 +0300

    changes from sule's app

--- app/Components/NewTask.tsx
+++ app/Components/NewTask.tsx
@@ -1,43 +1,43 @@
-import { useState } from 'react';
+import { useState } from "react";
 const NewTask = ({ handleDelete, handleAdd, project, tasks }) => {
-  const [enteredTask, setEnteredTask] = useState('');
+  const [enteredTask, setEnteredTask] = useState("");
 
   const handleChange = (event) => {
     setEnteredTask(event.target.value);
   };
 
   const handleClick = () => {
-    if (enteredTask.trim() === '') {
+    if (enteredTask.trim() === "") {
       return;
     }
 
     handleAdd(enteredTask);
-    setEnteredTask('');
+    setEnteredTask("");
   };
   return (
-    <div className='flex justify-between  '>
+    <div className="flex justify-between  ">
       <input
-        type='text'
-        className='input input-bordered w-1/2 px-2
-            py-1  bg-stone-200 h-10'
+        type="text"
+        className="input input-bordered w-1/2 px-2
+            py-1  bg-stone-200 h-10 text-gray-950"
         onChange={handleChange}
         value={enteredTask}
       />
       <button
         onClick={handleClick}
-        className='
-        hover:text-green-500'
+        className="
+        hover:text-green-500"
       >
         <svg
-          xmlns='http://www.w3.org/2000/svg'
-          viewBox='0 0 24 24'
-          fill='currentColor'
-          class='w-6 h-6'
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          class="w-6 h-6 text-black   hover:text-green-500 "
         >
           <path
-            fill-rule='evenodd'
-            d='M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 9a.75.75 0 0 0-1.5 0v2.25H9a.75.75 0 0 0 0 1.5h2.25V15a.75.75 0 0 0 1.5 0v-2.25H15a.75.75 0 0 0 0-1.5h-2.25V9Z'
-            clip-rule='evenodd'
+            fill-rule="evenodd"
+            d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 9a.75.75 0 0 0-1.5 0v2.25H9a.75.75 0 0 0 0 1.5h2.25V15a.75.75 0 0 0 1.5 0v-2.25H15a.75.75 0 0 0 0-1.5h-2.25V9Z"
+            clip-rule="evenodd"
           />
         </svg>
       </button>
--- app/Components/nav.tsx
+++ app/Components/nav.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import Button from './Button';
+import React from "react";
+import Button from "./Button";
 
 interface NavProps {
   newproject: {
@@ -23,21 +23,21 @@ const nav: React.FC<NavProps> = ({
   selectedProjectId,
 }) => {
   return (
-    <section className='navbar-start mt-12 bg-neutral py-8 rounded-r-lg rounded-br-md h-screen w-1/5 space-y-5 bg-gray-700'>
-      <h2 className=' font-bold text-2xl mb-3 text-gray-300 text-3xl mt-6 text-left'>
+    <section className="navbar-start mt-12 bg-neutral py-8 rounded-r-lg rounded-br-md h-screen w-1/5 space-y-5 bg-gray-700">
+      <h2 className=" font-bold text-2xl mb-3 text-gray-300 text-3xl mt-6 text-left">
         YOUR PROJECTS
       </h2>
       <Button isHomePage={false} addproject={newproject}>
         +Add Project
       </Button>
-      <ul className='my-8'>
+      <ul className="my-8">
         {projects.map((project) => {
           let btnStyle =
-            'w-full text-justify text-lg px-2 py-1 rounded-sm my-1  hover:text-stone-200 hover:font-bold hover:bg-slate-600';
+            "w-full text-justify text-lg px-2 py-1 rounded-sm my-1  hover:text-stone-200 hover:font-bold hover:bg-slate-600";
 
           if (project.id === selectedProjectId)
-            btnStyle += ' bg-gray-950 text-gray-200';
-          else btnStyle += ' text-gray-400';
+            btnStyle += " bg-gray-950 text-gray-200";
+          else btnStyle += " text-gray-400";
 
           return (
             <li key={project.id}>
@@ -45,7 +45,7 @@ const nav: React.FC<NavProps> = ({
                 className={btnStyle}
                 onClick={() => selectProject(project.id)}
               >
-                {project.title}
+                {project.title.toUpperCase()}
               </button>
             </li>
           );
